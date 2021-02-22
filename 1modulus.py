d22b85f79fbed322f7ea010d8011f7f722a00d7b
boilinlinz <boilinlinz@gmail.com>
2021-02-23 06:48:59 +0800

    converted into a function

--- 1modulus.py
+++ 1modulus.py
@@ -1,8 +1,5 @@
 # This is an attempt to recreating the % modulo operator
 # 
-
-a = abs(int(input()))
-b = abs(int(input()))
 """
  Find the remainder of a/b
 
@@ -21,15 +18,24 @@ ex. 5/3 -> 3x1, 3x2, 3x3, 3x4..., -> g = 3x1 because 3x2 is > 5
 2. Subtract the number (g) to the dividend(a) to get the remainder |   5 - g -> 5 - (3x1) -> 5-3 -> remainder = 2
 3. if the divisor(b) can be multiplied to be equal the dividend(a), then the remainder is zero 
 """
+def makeMod(a=0, b=0):
+    try:
+        a = abs(int(a))
+        b = abs(int(b))
+        g = 1
+        for x in range(a+1):
+            h = b * x 
+            if h < a:   # Finds the highest number that when * to b is less than a
+                g = h
+                c = a - g   #Subtract highest number to the dividend
+            elif h == a:   #If b can be multiplied to be equal to a, remainder is 0 
+                c = 0
+        return c
+    except: 
+        return None
 
-
-g = 0 
-for x in range(a+1):
-    h = b * x 
-    if h < a:   # Finds the highest number that when * to b is less than a
-        g = h
-        c = a - g   #Subtract highest number to the dividend
-    elif h == a:   #If b can be multiplied to be equal to a, remainder is 0 
-       c = 0
-
-print( "The remainder of " + str(a) + "/" +str(b) + " is " + str(c))
+a = input("Dividend: ")
+b = input("Divsor: ")
+c = makeMod(a,b)
+#print("remainder of {}/{} is {}".format(a,b,c))
+print(f"The remainder of {a}/{b} is {c}")
