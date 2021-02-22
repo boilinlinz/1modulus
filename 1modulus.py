f1b0dab1d5385b1b0ebf18e54536ed2401ef2fd1
boilinlinz <boilinlinz@gmail.com>
2021-02-22 18:22:04 +0800

    proj finish

--- 1modulus.py
+++ 1modulus.py
@@ -1,1 +1,35 @@
-# Test
+# This is an attempt to recreating the % modulo operator
+# 
+
+a = abs(int(input()))
+b = abs(int(input()))
+"""
+ Find the remainder of a/b
+
+ Find the largest number(c) (that is <b) that can be divided to a.
+ if < 1, return a (a<b)
+ if c == a, return a
+ if a < b, remainder is a
+
+ex. 5/3 = 1 rem.2
+    25/5 = 5 rem.0
+    25/26 0 rem.25
+process outline:
+a/b = c
+1. Find highest number (g) that when multiplied to the divisor(b) is less than the dividend(a)
+ex. 5/3 -> 3x1, 3x2, 3x3, 3x4..., -> g = 3x1 because 3x2 is > 5
+2. Subtract the number (g) to the dividend(a) to get the remainder |   5 - g -> 5 - (3x1) -> 5-3 -> remainder = 2
+3. if the divisor(b) can be multiplied to be equal the dividend(a), then the remainder is zero 
+"""
+
+
+g = 0 
+for x in range(a+1):
+    h = b * x 
+    if h < a:   # Finds the highest number that when * to b is less than a
+        g = h
+        c = a - g   #Subtract highest number to the dividend
+    elif h == a:   #If b can be multiplied to be equal to a, remainder is 0 
+       c = 0
+
+print( "The remainder of " + str(a) + "/" +str(b) + " is " + str(c))
